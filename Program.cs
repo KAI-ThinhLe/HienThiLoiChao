0e7bb92c61b655e5ac07354bfb878a3cba7c18ea
unknown <legiathinh0508@gmail.com>
2024-08-07 17:47:59 +0700

    HienThiLoiChao

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloName 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yêu cầu người dùng nhập tên
+            Console.WriteLine("Enter your name: ");
+            
+            // Đọc tên từ bàn phím
+            string yourName = Console.ReadLine();
+            
+            // Hiển thị lời chào kèm tên
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
